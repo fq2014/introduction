cdbea1b1f43e184ced18978b2675d0ff03426be9
fq2014 <78662982+fq2014@users.noreply.github.com>
2021-02-22 23:58:53 -0500

    Update solution.py

--- webserver/solution.py
+++ webserver/solution.py
@@ -2,7 +2,7 @@ from socket import *
 import sys
 def webServer(port=13331):
    serverSocket = socket(AF_INET, SOCK_STREAM)
-   serverSocket.bind(("", port))
+   serverSocket.bind(('', port))
    serverSocket.listen(1)
    while True:
 	  
