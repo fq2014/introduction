69182b0a1ce28f5fa52d2df0fb35758911516674
fq2014 <78662982+fq2014@users.noreply.github.com>
2021-02-22 22:07:27 -0500

    Add files via upload

--- webserver/solution.py
+++ webserver/solution.py
@@ -2,55 +2,65 @@ from socket import *
 
 import sys
 
+
+
 def webServer(port=13331):
 
-    serverSocket = socket(AF_INET, SOCK_STREAM)
+   serverSocket = socket(AF_INET, SOCK_STREAM)
 
-    serverSocket.bind(("", port))
+   serverSocket.bind(("", port))
 
-    serverSocket.listen(1)
-
-    while True:
-
-        #print('Ready to serve...')
-
-        connectionSocket, addr = serverSocket.accept()
-
-        try:
-            try:
-
-                message = connectionSocket.recv(1024)
-
-                filename = message.split()[1]
-
-                f = open(filename[1:])
-
-                outputdata = f.read()
-
-                connectionSocket.send("HTTP/1.1 200 OK\r\n\r\n".encode())
+   serverSocket.listen(1)
 
 
-                for i in range(0, len(outputdata)):
-                    connectionSocket.send(outputdata[i].encode())
+   while True:
+
+
+       print('Ready to serve...')
+
+       connectionSocket, addr = serverSocket.accept()
+
+       try:
+
+           message = connectionSocket.recv(1024)
+
+           filename = message.split()[1]
+
+           f = open(filename[1:])
+
+           outputdata = f.read()
 
 
 
-                connectionSocket.send("\r\n".encode())
-                connectionSocket.close()
 
-            except IOError:
-        except (ConnectionResetError, BrokenPipeError): pass
 
-    connectionSocket.send("HTTP/1.1 404 Not Found\r\n\r\n".encode())
+           connectionSocket.send("HTTP/1.1 200 OK\r\n\r\n".encode())
 
-    connectionSocket.close()
 
-    serverSocket.close()
 
-    sys.exit()
+
+           for i in range(0, len(outputdata)):
+               connectionSocket.send(outputdata[i].encode())
+
+
+
+           connectionSocket.send("\r\n".encode())
+
+           connectionSocket.close()
+
+       except IOError:
+
+
+           connectionSocket.send("HTTP/1.1 404 Not Found\r\n\r\n".encode())
+
+
+           connectionSocket.close()
+
+   serverSocket.close()
+
+   sys.exit()
 
 
 
 if __name__ == "__main__":
-
-   webServer(13331)
+    webServer(13331)
