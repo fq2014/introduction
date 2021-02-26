f054e734ad8fd413a697a1fba2b8bacdf6243d50
fq2014 <78662982+fq2014@users.noreply.github.com>
2021-02-25 20:24:53 -0500

    Add files via upload

--- solution.py
+++ solution.py
@@ -1,33 +1,38 @@
-from socket import *
-import sys
-def webServer(port=13331):
-   serverSocket = socket(AF_INET, SOCK_STREAM)
-   serverSocket.bind(('', port))
-   serverSocket.listen(1)
-   while True:
-	  
-       #print('Ready to serve...')
-       connectionSocket, addr = serverSocket.accept()
-       try:
-           message = connectionSocket.recv(1024)
-           filename = message.split()[1]
-           f = open(filename[1:])
-           outputdata = f.read()
-           f.close()
-		  
-           connectionSocket.send("HTTP/1.1 200 OK\r\n\r\n".encode())
-           for i in range(0, len(outputdata)):
-               connectionSocket.send(outputdata[i].encode())
-           connectionSocket.send("\r\n".encode())
-           connectionSocket.close()
-       except IOError:
-           connectionSocket.send("HTTP/1.1 404 Not Found\r\n\r\n".encode())
-           connectionSocket.close()
-		  
-       except BrokenPipeError:
-           break
-		  
-   serverSocket.close()
-   sys.exit()
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb  6 15:27:02 2021
+
+@author: Faisal Qasim
+"""
+
+### welcome_assignment_answers
+### Input - All eight questions given in the assignment.
+### Output - The right answer for the specific question.
+
+def welcome_assignment_answers(question):
+    #The student doesn't have to follow the skeleton for this assignment.
+    #Another way to implement is using a "case" statements similar to C.
+    if question == "Are encoding and encryption the same? - Yes/No":
+        answer = "No"
+    elif question == "Is it possible to decrypt a message without a key? - Yes/No":
+        answer = "No"
+    elif question == "Is it possible to decode a message without a key? - Yes/No":
+        answer = "Yes"
+    elif question == "Is a hashed message supposed to be un-hashed? - Yes/No":
+        answer = "No"
+    elif question == "What is the MD5 hashing value to the following message: 'NYU Computer Networking' - Use MD5 hash generator and use the answer in your code":
+        answer = "42b76fe51778764973077a5a94056724"
+    elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
+        answer = "no"
+    elif question == "What layer from the TCP/IP model the protocol DHCP belongs to? - The answer should be a numeric number":
+        answer = 5
+    elif question == "What layer of the TCP/IP model the protocol TCP belongs to? - The answer should be a numeric number":
+        answer = 4
+    return(answer)
+# Complete all the questions.
+
+
 if __name__ == "__main__":
-    webServer(port=13331)
+    #use this space to debug and verify that the program works
+    debug_question = "Are encoding and encryption the same? - Yes/No"
+    print(welcome_assignment_answers(debug_question))
